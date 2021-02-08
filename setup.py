1d21aecd0c800eb9a587b4525e752750e90dd3be
Johannes Englisch <englisch@shh.mpg.de>
2021-02-08 13:28:48 +0100

    cldfbench

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    name='cldfbench_sidaama',
+    py_modules=['cldfbench_sidaama'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'cldfbench.dataset': [
+            'sidaama=cldfbench_sidaama:Dataset',
+        ]
+    },
+    install_requires=[
+        'cldfbench',
+    ],
+    extras_require={
+        'test': [
+            'pytest-cldf',
+        ],
+    },
+)
